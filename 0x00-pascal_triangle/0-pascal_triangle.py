c612030a06760b4da9848d275de551fbd5130c55
L <sebasgarzons88@gmail.com>
2022-07-28 23:11:46 -0500

    Pycodestyle and fixed if with condition requirement and execution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,16 +2,16 @@ def pascal_triangle(n):
 
     if n <= 0:
         return[]
-    else:
-        triangle_index = [[1]]
-        while len(triangle_index) != n:
 
-            adjacent = triangle_index[-1]
-            actual = [1]
+    triangle_index = [[1]]
+    while len(triangle_index) != n:
 
-            for x in range(len(adjacent) - 1):
-                actual.append(adjacent[x] + adjacent[x + 1])
-            actual.append(1)
-            triangle_index.append(actual)
+        adjacent = triangle_index[-1]
+        actual = [1]
 
-        return triangle_index
+        for x in range(len(adjacent) - 1):
+            actual.append(adjacent[x] + adjacent[x + 1])
+        actual.append(1)
+        triangle_index.append(actual)
+
+    return triangle_index
