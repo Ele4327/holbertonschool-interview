30fc05fed4e8b58d25f1f2d0d7936ed9f5fb6b58
L <sebasgarzons88@gmail.com>
2022-07-28 23:01:41 -0500

    Update n<=0 requirement

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,17 @@
 def pascal_triangle(n):
 
-    triangle_index = [[1]]
-    while len(triangle_index) != n:
+    if n <= 0:
+        return[]
+    else:
+        triangle_index = [[1]]
+        while len(triangle_index) != n:
 
-        adjacent = triangle_index[-1]
-        actual = [1]
+            adjacent = triangle_index[-1]
+            actual = [1]
 
-        for x in range(len(adjacent) - 1):
-            actual.append(adjacent[x] + adjacent[x + 1])
-        actual.append(1)
-        triangle_index.append(actual)
+            for x in range(len(adjacent) - 1):
+                actual.append(adjacent[x] + adjacent[x + 1])
+            actual.append(1)
+            triangle_index.append(actual)
 
-    return triangle_index
+        return triangle_index
