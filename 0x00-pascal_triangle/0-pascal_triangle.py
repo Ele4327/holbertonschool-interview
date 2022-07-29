8b14c8e3f64110e6f16c5dc680b4a76c5c35bca0
L <sebasgarzons88@gmail.com>
2022-07-28 22:52:25 -0500

    Directory created

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,14 @@
+def pascal_triangle(n):
+
+    triangle_index = [[1]]
+    while len(triangle_index) != n:
+
+        adjacent = triangle_index[-1]
+        actual = [1]
+
+        for x in range(len(adjacent) - 1):
+            actual.append(adjacent[x] + adjacent[x + 1])
+        actual.append(1)
+        triangle_index.append(actual)
+
+    return triangle_index
