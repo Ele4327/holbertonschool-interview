ec4641f4c012b61cc09ce0c926667f26207cd02c
L <sebasgarzons88@gmail.com>
2022-08-04 22:55:18 -0500

    LockBoxes and Documentation PY

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+    Function that determine if all the boxes can be opened.
+    In n number of locked boxes, each box is numbered sequentially
+    from 0 to n - 1 and each box may contain keys to the other boxes.
+"""
+
+
+def canUnlockAll(boxes):
+    """ Method that compare the total len of the boxes.
+    Compare them according his number and position. """
+
+    for key_value in range(1, len(boxes) - 1):
+
+        for position_box in range(len(boxes)):
+
+            exist = (key_value in boxes[position_box]
+                     and key_value != position_box)
+
+            if exist:
+                break
+
+        if exist is False:
+            return exist
+
+    return True
