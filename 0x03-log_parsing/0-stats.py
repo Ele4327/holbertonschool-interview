f434a47b99fbde2b67e46cfe8bfa13f3613eb745
L <sebasgarzons88@gmail.com>
2022-08-18 19:31:50 -0500

    Log Parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,7 +1,48 @@
+#!/usr/bin/python3
+"""
+    Write a script that reads stdin line by line and computes metrics
+"""
+
+import sys
+
 dict_status_code = {
-    200, 301, 400, 401, 403, 404, 405 and 500
+    '200': 0, '301': 0, '400': 0, '401': 0,
+    '403': 0, '404': 0, '405': 0, '500': 0
 }
 
-size = 0;
+size = 0
 
-cont_lines = 0;
+cont_lines = 0
+
+if __name__ == "__main__":
+    try:
+        for line in sys.stdin:
+            log = line.split(" ")
+
+            if len(log) > 2:
+                code_value = log[-2]
+                size_val_temp = int(log[-1])
+
+                if code_value in dict_status_code.keys():
+                    dict_status_code[code_value] = \
+                        dict_status_code[code_value] + 1
+                size = size + size_val_temp
+                cont_lines = cont_lines + 1
+
+            if (cont_lines % 10) == 0:
+                print("File size: {:d}".format(size))
+
+                for key in sorted(dict_status_code.keys()):
+                    if dict_status_code[key] == 0:
+                        continue
+                    print("{}: {}".format(key, dict_status_code[key]))
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print("File size: {}".format(size))
+        for key in sorted(dict_status_code.keys()):
+            if dict_status_code[key] == 0:
+                continue
+            print("{}: {}".format(key, dict_status_code[key]))
