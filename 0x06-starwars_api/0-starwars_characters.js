3efaa8bbffb66201e1a538d5be101d30767a7d6c
L <sebasgarzons88@gmail.com>
2022-09-14 19:57:44 -0400

    After corrections - Never async without await

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,24 +3,24 @@
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv.slice(2);
 
-const GetData = (list, index) => {
-  request.get(list[index], (error, res, body) => {
-    const ListSize = list.length;
+const getData = (list1, index) => {
+  request.get(list1[index], (error, response, body) => {
+    const ListSize = list1.length;
 
     if (error) throw error;
     const ReturnValue = JSON.parse(body);
     console.log(ReturnValue.name);
 
     if (index < (ListSize - 1)) {
-      GetData(list, (index + 1));
+      getData(list1, (index + 1));
     }
   });
 };
 
-request.get(url, async (error, res) => {
+request.get(url, (error, response) => {
   if (error) console.log(error);
   else {
-    const list = JSON.parse(res.body);
-    GetData(list.characters, 0);
+    const list2 = JSON.parse(response.body);
+    getData(list2.characters, 0);
   }
 });
